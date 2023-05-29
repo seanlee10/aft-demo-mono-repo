b6b038feaa56b2ad06f21574291c95f874b5cf82
Sean Lee <seule@amazon.com>
2023-05-30 03:50:29 +0900

    change policy paths

--- modules/guardrails/main.tf
+++ modules/guardrails/main.tf
@@ -1,9 +1,9 @@
 resource "aws_organizations_policy" "prevent_disabling_or_changing_aws_config" {
   name    = "prevent_disabling_or_changing_aws_config"
-  content = file("./policies/prevent_disabling_or_changing_aws_config.json")
+  content = file("policies/prevent_disabling_or_changing_aws_config.json")
 }
 
 resource "aws_organizations_policy" "prevent_deleting_vpc_flow_logs" {
   name    = "prevent_deleting_vpc_flow_logs"
-  content = file("./policies/prevent_deleting_vpc_flow_logs.json")
+  content = file("policies/prevent_deleting_vpc_flow_logs.json")
 }
